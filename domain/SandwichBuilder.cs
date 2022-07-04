b0d07683130c949d7f32a50263e4c60e7d1ab3ef
Adem Mrizak <adem.mrizak@gmail.com>
2022-07-04 10:34:35 +0200

    Sandwich Builder

---
+++ domain/SandwichBuilder.cs
@@ -0,0 +1,38 @@
+﻿namespace Domain.Core;
+
+public class SandwichBuilder
+{
+    private string Name;
+    private Dictionary<Ingredient, Quantity> Ingredients;
+    private Price Price;
+
+    public SandwichBuilder()
+    {
+        Name = "";
+        Ingredients = new Dictionary<Ingredient, Quantity>();
+        Price = new(0, "");
+    }
+
+    public SandwichBuilder WithName(string name)
+    {
+        Name = name;
+        return this;
+    }
+
+    public SandwichBuilder WithIngredient(Ingredient ingredient, Quantity quantity)
+    {
+        Ingredients.Add(ingredient, quantity);
+        return this;
+    }
+
+    public SandwichBuilder WithPrice(Price price)
+    {
+        Price = price;
+        return this;
+    }
+
+    public Sandwich Build()
+    {
+        return new Sandwich(Name, Price, Ingredients);
+    }
+}
